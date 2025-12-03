e3e23d9e5be384de5118caaa1f12a9d7d55a5b92
arturocesarorozco-sys <arturocesarorozco@gmail.com>
2025-12-03 01:44:33 -0700

    fix

--- recetarium/backend-recetario/src/server.js
+++ recetarium/backend-recetario/src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import mysql from 'mysql2'
+import mysql from 'mysql2/promise';
 import recetasRoutes from './routes/recetas.js';
 import usuariosRoutes from './routes/usuarios.js';
 import misRecetasRoutes from './routes/misrecetas.js'; 
 import agregarRecetasRoutes from './routes/agregarrecetas.js'; // <-- Importa tu nuevo archivo
 import semanaRoutes from './routes/semana.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -13,21 +16,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+
+// Crear pool de conexiones
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect(err => {
-  if (err) {
-    console.error("Error al conectar a MySQL:", err);
-    return;
-  }
-  console.log("Conectado a MySQL");
+// Middleware para añadir db a req
+app.use((req, res, next) => {
+  req.db = db;
+  next();
 });
+
 
 // Rutas principales
 app.use('/api/recetas', recetasRoutes);
