ab7b6fbb27329eab6c2894665784fae170dce190
arturocesarorozco-sys <arturocesarorozco@gmail.com>
2025-12-03 02:12:05 -0700

    fixx

--- recetarium/backend-recetario/src/config/db.js
+++ recetarium/backend-recetario/src/config/db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config(); // carga .env local o variables de Render
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'ACOZ1023',   
-  database: 'aplicacion',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
